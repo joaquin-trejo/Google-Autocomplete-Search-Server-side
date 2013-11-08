02a4fa30ec32b15daf96225545cd675dfd19f14a
pradeep singh <er.pradeep007@gmail.com>
2013-11-08 11:32:45 +0530

    Update autocomplete.php
    restricting google search to city specific autosuggest

--- autocomplete.php
+++ autocomplete.php
@@ -7,39 +7,49 @@ Google autocomplete api (json) will be used to fetch data.
 */
 
 Class Autocomplete{
+  
+  private $api_key = '';
 
+
+  function __construct() {
+	$this->api_key = '<API KEY>';
+  }
+  
   public function suggest_location(){
         header("Cache-Control: private, max-age=86400");
-			  header("Expires: ".gmdate('r', time()+86400));
-        $query = 'pune '.$this->input->get('q',TRUE);
-	    	$apikey='<API KEY>';
-	    	$url = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?key='.$apikey.'&types=geocode&sensor=true&language=en&location=18.526817,73.856564&radius=12000&input='.urlencode($query);
-	    	$ch = curl_init();
-	    	curl_setopt($ch, CURLOPT_URL, $url);
-	    	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	    	$data1 = curl_exec($ch);
-	    	curl_close($ch);
-	    	$details = json_decode($data1, true);
-	    	header("Content-Type: application/json");
-	    	$json =  "{\"results\": [";
-	    	foreach($details['predictions'] as $key=>$row) {
-	    		$arr[] = "{\"id\": \"".$row['reference']."\", \"value\": \"".$row['description']."\"}";
-	    	}
-	    	$json .= implode(", ", $arr);
-	    	echo $json . "]}";
+	header("Expires: ".gmdate('r', time()+86400));
+        $query = $_GET["cityname"].' '.$_GET["q"];
+        $city_center_latlng = $_GET["city_center_latlng"];
+    	$apikey = $this->api_key;
+    	$url = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?key='.$apikey.'&types=geocode&sensor=true&language=en&location='.$city_center_latlng.'&radius=12000&input='.urlencode($query);
+    	$ch = curl_init();
+    	curl_setopt($ch, CURLOPT_URL, $url);
+    	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    	$data1 = curl_exec($ch);
+    	curl_close($ch);
+    	$details = json_decode($data1, true);
+    	header("Content-Type: application/json");
+    	$json =  "{\"results\": [";
+    	foreach($details['predictions'] as $key=>$row) {
+    		$arr[] = "{\"id\": \"".$row['reference']."\", \"value\": \"".$row['description']."\"}";
+    	}
+    	$json .= implode(", ", $arr);
+    	echo $json . "]}";
   
   }
   public function get_geocode(){
-      $query = 'https://maps.googleapis.com/maps/api/place/details/json?reference='.urlencode($this->input->get('location',TRUE)).'&sensor=true&key=AIzaSyAt_gCyokXmulA4L33EWKg_SrR34c6t7dU';
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, $query);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			$data = curl_exec($ch); // execute curl session
-			curl_close($ch); // close curl session
-			$details = json_decode($data, true);
-	    $map['lat'] = $details['result']['geometry']['location']['lat'];
-	    $map['long'] = $details['result']['geometry']['location']['lng'];
-	    return json_encode($map);
+  	$apikey = $this->api_key;
+  	$reference = $_GET["reference"];
+        $query = 'https://maps.googleapis.com/maps/api/place/details/json?reference='.urlencode($reference).'&sensor=true&key='.$apikey;
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $query);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	$data = curl_exec($ch); // execute curl session
+	curl_close($ch); // close curl session
+	$details = json_decode($data, true);
+	$map['lat'] = $details['result']['geometry']['location']['lat'];
+	$map['long'] = $details['result']['geometry']['location']['lng'];
+	return json_encode($map);
   }
 
 
